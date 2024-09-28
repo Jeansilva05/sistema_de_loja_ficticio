eb0a0346932a0ea509496fe7dad34cd96f83661a
Jeansilva05 <jeanprg074@gmail.com>
2024-09-27 21:49:36 -0300

    feat: adicionar itens ao carrinho

--- src/Carrinho.java
+++ src/Carrinho.java
@@ -4,7 +4,12 @@ public class Carrinho {
 
     public void percorrerCarrinho() {
         for (int i = 0; i < this.indice; i++) {
-            System.out.println(produtos[i].codBarra);
+            System.out.println(this.produtos[i].codBarra);
         }
+    }
+
+    public void adicionarAoCarrinho(Produto prod){
+        this.produtos[this.indice] = prod;
+        this.indice++;
     }
 }

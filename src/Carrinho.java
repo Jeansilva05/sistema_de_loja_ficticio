fee15335694dca8e3eb5abbe2d39f82257648078
Jeansilva05 <jeanprg074@gmail.com>
2024-09-27 22:10:51 -0300

    cleanup: Carrinho.java

--- src/Carrinho.java
+++ src/Carrinho.java
@@ -2,7 +2,7 @@ public class Carrinho {
     public Produto[] produtos = new Produto[10];
     public int indice = 0;
 
-    public void percorrerCarrinho() {
+    public void consultarCarrinho() {
         for (int i = 0; i < this.indice; i++) {
             System.out.println(this.produtos[i].codBarra);
         }
